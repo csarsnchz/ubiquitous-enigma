c4cf406f132e0b14c7baab71ec722b585bbc1442
csarsnchz <csarsnchz@gmail.com>
2023-11-20 07:23:26 -0600

    Added sorting functions file
    Functions to made data sorting

---
+++ functions.py
@@ -0,0 +1,64 @@
+# Create a function for binary search
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 3))
+
+print(binary_search(my_list, -1))
+
+# Create a function for bubble sort
+def bubble_sort(list):
+    for i in range(len(list) - 1, 0, -1):
+        for j in range(i):
+            if list[j] > list[j + 1]:
+                temp = list[j]
+                list[j] = list[j + 1]
+                list[j + 1] = temp
+    return list
+
+my_list = [3, 6, 2, 8, 4, 1]
+
+print(bubble_sort(my_list))
+
+# Create a function for selection sort
+def selection_sort(list):
+    for i in range(len(list) - 1):
+        min_index = i
+        for j in range(i + 1, len(list)):
+            if list[j] < list[min_index]:
+                min_index = j
+        list[i], list[min_index] = list[min_index], list[i]
+    return list
+
+my_list = [3, 6, 2, 8, 4, 1]
+
+print(selection_sort(my_list))
+
+# Create a function for insertion sort
+def insertion_sort(list):
+    for i in range(1, len(list)):
+        key = list[i]
+        j = i - 1
+        while j >= 0 and key < list[j]:
+            list[j + 1] = list[j]
+            j -= 1
+        list[j + 1] = key
+    return list 
+
+my_list = [3, 6, 2, 8, 4, 1]   
+
+print(insertion_sort(my_list))
